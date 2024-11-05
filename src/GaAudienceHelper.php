fb8bef89cda3c2e28580b18ba8c697cca56a9751
Maarten <maarten.laene@gmail.com>
2024-11-05 12:58:28 +0100

    Sync audiences in GA4

--- src/GaAudienceHelper.php
+++ src/GaAudienceHelper.php
@@ -2,15 +2,47 @@
 
 namespace DMT\AbMiddleware;
 
+use Google\Analytics\Admin\V1alpha\AnalyticsAdminServiceClient;
+use Google\Analytics\Admin\V1alpha\Audience;
+use Google\Analytics\Admin\V1alpha\AudienceDimensionOrMetricFilter;
+use Google\Analytics\Admin\V1alpha\AudienceDimensionOrMetricFilter\StringFilter;
+use Google\Analytics\Admin\V1alpha\AudienceDimensionOrMetricFilter\StringFilter\MatchType;
+use Google\Analytics\Admin\V1alpha\AudienceFilterClause;
+use Google\Analytics\Admin\V1alpha\AudienceFilterExpression;
+use Google\Analytics\Admin\V1alpha\AudienceFilterExpressionList;
+use Google\Analytics\Admin\V1alpha\AudienceFilterScope;
+use Google\Analytics\Admin\V1alpha\AudienceSimpleFilter;
+use Google\Analytics\Admin\V1alpha\SubpropertyEventFilterClause\FilterClauseType;
 use InvalidArgumentException;
 
 class GaAudienceHelper
 {
     public function __construct(
         private readonly AbService $abService,
-        private readonly string $gaId,
+        private readonly AnalyticsAdminServiceClient $client,
         private readonly string $audiencePrefix = 'DOAB',
+        private readonly string $accountId,
+        private readonly array $propertyIds,
     ) {
+    }
+
+    public function getPropertyIds(): array
+    {
+        return $this->propertyIds;
+    }
+
+    public function getAudienceNames(): array
+    {
+        $experiments = $this->abService->getExperiments();
+
+        $audienceNames = [];
+        foreach ($experiments as $experiment => $variants) {
+            foreach(array_keys($variants) as $variant) {
+                $audienceNames[] = implode('-', [$this->audiencePrefix, $experiment, $variant]);
+            }
+        }
+
+        return $audienceNames;
     }
 
     /**
@@ -23,24 +55,30 @@ class GaAudienceHelper
         // synchronize audiences with Google Analytics
         $experiments = $this->abService->getExperiments();
         $audiences = $this->getAudiences();
+        $audienceNames = [];
+        foreach ($this->propertyIds as $propertyId) {
+            $audienceNames += array_column($audiences[$propertyId], 'name');
+        }
 
-        $experimentAudienceIds = [];
-
+        $experimentAudienceNames = [];
+        
         foreach ($experiments as $experiment => $variants) {
             foreach(array_keys($variants) as $variant) {
-                $audienceId = implode('-', [$this->audiencePrefix, $experiment, $variant]);
+                $audienceName = implode('-', [$this->audiencePrefix, $experiment, $variant]);
 
-                $experimentAudienceIds[] = $audienceId;
-
-                if (!array_key_exists($audienceId, $audiences)) {
-                    $this->createAudience($audienceId);
+                $experimentAudienceNames[] = $audienceName;
+                if (!in_array($audienceName, $audienceNames)) {
+                    foreach ($this->propertyIds as $propertyId) {
+                        $this->createAudience($audienceName, $propertyId);
+                    }
                 }
             }
         }
-
-        foreach ($audiences as $audienceId => $audience) {
-            if (!in_array($audienceId,$experimentAudienceIds)) {
-                $this->archiveAudience($audienceId);
+        foreach ($this->propertyIds as $propertyId) {
+            foreach ($audiences[$propertyId] as $audience) {
+                if (!in_array($audience['name'],$experimentAudienceNames)) {
+                    $this->archiveAudience($audience['id']);
+                }
             }
         }
     }
@@ -52,29 +90,74 @@ class GaAudienceHelper
      */
     public function getAudiences(): array
     {
-        // FILTER THIS ON $this->audiencePrefix
-        return [
-            'audience1',
-            'audience2',
-        ];
+        foreach ($this->propertyIds as $propertyId) {
+            $audienceList =  $this->client->listAudiences('properties/' . $propertyId);
+            foreach ($audienceList as $audience) {
+                $name = $audience->getDisplayName();
+                if(str_starts_with($name, $this->audiencePrefix)) {
+                    $audiences[$propertyId][] = ['name' => $name, 'id' => $audience->getName()];
+                }
+            }
+        }
+        return $audiences ?? [];
     }
 
     public function archiveAudience(string $audienceId): void
     {
-        if (!str_starts_with($audienceId, $this->audiencePrefix)) {
+        $audience = $this->client->getAudience($audienceId);
+        $audienceName = $audience->getDisplayName();
+        if (!str_starts_with($audienceName, $this->audiencePrefix)) {
             throw new InvalidArgumentException('Audience ID does not start with the configured prefix');
         }
-
-        // archive audience in Google Analytics
+        $this->client->archiveAudience($audienceId);
     }
 
-    public function createAudience(string $audienceId): void
+    /**
+     * @throws Google\ApiCore\ApiException
+     */
+    public function createAudience(string $audienceName, string $propertyId): void
     {
-        if (!str_starts_with($audienceId, $this->audiencePrefix)) {
+        if (!str_starts_with($audienceName, $this->audiencePrefix)) {
             throw new InvalidArgumentException('Audience ID does not start with the configured prefix');
         }
+        
+        $stringFilter = new StringFilter();
+        $stringFilter->setValue($audienceName);
+        $stringFilter->setMatchType(MatchType::EXACT);
+        
+        $filter = new AudienceDimensionOrMetricFilter();
+        $filter->setFieldName('experienceVariantId');
+        $filter->setStringFilter($stringFilter);
 
-        // create audience in Google Analytics
+        $filterExpression = new AudienceFilterExpression();
+        $filterExpression->setDimensionOrMetricFilter($filter);
+
+        $audienceFilterExpressionList = new AudienceFilterExpressionList();
+        $audienceFilterExpressionList->setFilterExpressions([$filterExpression]);
+
+        $filterExpression = new AudienceFilterExpression();
+        $filterExpression->setOrGroup($audienceFilterExpressionList);
+
+        $audienceFilterExpressionList = new AudienceFilterExpressionList();
+        $audienceFilterExpressionList->setFilterExpressions([$filterExpression]);
+
+        $filterExpression = new AudienceFilterExpression();
+        $filterExpression->setAndGroup($audienceFilterExpressionList);
+
+        $simpleFilter = new AudienceSimpleFilter();
+        $simpleFilter->setFilterExpression($filterExpression);
+        $simpleFilter->setScope(AudienceFilterScope::AUDIENCE_FILTER_SCOPE_ACROSS_ALL_SESSIONS);
+
+        $filterClause = new AudienceFilterClause();
+        $filterClause->setSimpleFilter($simpleFilter);
+        $filterClause->setClauseType(FilterClauseType::PBINCLUDE);
+
+        $audience = new Audience();
+        $audience->setDisplayName($audienceName);
+        $audience->setDescription('AB Test audience for ' . $audienceName);
+        $audience->setFilterClauses([$filterClause]);
+        $audience->setMembershipDurationDays(30);
+        $response = $this->client->createAudience('properties/' . $propertyId, $audience);
     }
 
     /**
